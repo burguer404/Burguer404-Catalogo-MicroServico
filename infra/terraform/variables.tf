b8c355a4f360f85d2873fcb2726be3e3366d34ae
Moises Loesch <moisesloesch.loesch1@gmail.com>
2025-12-04 23:02:08 -0300

    removendo dependência de artefatos

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -1,7 +1,7 @@
 variable "resource_group_name" {
   description = "Nome do Resource Group no Azure"
   type        = string
-  default     = "rg-burguer404"
+  default     = "rg-burguer404-Catalogo"
 }
 
 variable "location" {
@@ -13,13 +13,13 @@ variable "location" {
 variable "aks_cluster_name" {
   description = "Nome do cluster AKS"
   type        = string
-  default     = "aks-burguer404"
+  default     = "aks-burguer404-Catalogo"
 }
 
 variable "dns_prefix" {
   description = "Prefixo DNS para o cluster AKS"
   type        = string
-  default     = "aksburguer404"
+  default     = "aksburguer404Catalogo"
 }
 
 variable "node_count" {
